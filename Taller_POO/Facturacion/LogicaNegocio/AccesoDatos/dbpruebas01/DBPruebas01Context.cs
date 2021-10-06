be23386569c8395f7cf4e9761773100f15b287df
onivia <ohnivia@gmail.com>
2021-10-06 15:49:42 -0500

    Taller Pilares de la POO

--- Taller_POO/Facturacion/LogicaNegocio/AccesoDatos/dbpruebas01/DBPruebas01Context.cs
+++ Taller_POO/Facturacion/LogicaNegocio/AccesoDatos/dbpruebas01/DBPruebas01Context.cs
@@ -27,7 +27,7 @@ namespace LogicaNegocio.AccesoDatos.dbpruebas01
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=190.60.208.196,9191;User ID=usuario00;Password=UEAN00$.;Initial Catalog=dbpruebas01;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False");
+                
             }
         }
 
